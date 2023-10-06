506a3fc215b6313981f33d1036585a44b53a4624
Mthabs <>
2023-10-06 21:02:56 +0000

    add post form class and init

--- chats/forms.py
+++ chats/forms.py
@@ -1,7 +1,23 @@
 from .models import Comment
 from django import forms
+from .models import Post
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'author', 'Content',
+                  'featured_image', 'excerpt', 'status', ]
+        widgets = {
+            'excerpt': forms.Textarea(attrs={'rows': 4, 'cols': 30}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.fields['status'].initial = 1
+        self.fields['title'].widget.attrs.update({'id': 'id_title'})
+        self.fields['slug'].widget.attrs.update({'id': 'id_slug'})
