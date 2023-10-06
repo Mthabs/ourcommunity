fdc543925752a17c84369dd0ef31bced7d2cf9f1
Mthabs <>
2023-10-06 20:58:15 +0000

    add url path for adding post

--- chats/urls.py
+++ chats/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('add/', views.PostCreate.as_view(), name='post_add'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
